ae3029cdd48b8a61189eaeb4f362c24d684c341d
Yevhen <skaretskiy1999@gmail.com>
2024-03-11 13:22:12 +0200

    Smth

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 	return (
 		<main className='container flex justify-between w-full'>
 			<>
-				<h1>Home</h1>
+				<h1 className='font-medium'>Home</h1>
 			</>
 		</main>
 	);
